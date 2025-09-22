4dd5c7eae86e8e1b955054ebee3c49ce56bfc35e
Luis Angel Yuman Rodas <lyumanr@miumg.edu.gt>
2025-09-21 18:44:03 -0600

    Ejemplos de Enlace y estilos

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 const { createApp } = Vue;
 
-// Ejemplo con propiedad computada
+// Enlace de clases: Sintaxis de objeto
 createApp({
     data() {
-        return { username: '' }
-    },
-    computed: {
-        welcomeMessage() {
-            return this.username
-                ? `¡Bienvenido, ${this.username}!`
-                : 'Por favor, escribe tu nombre.';
-        }
+        return { isActive: false }
     }
-}).mount('#welcome-computed');
+}).mount('#class-object');
 
-// Ejemplo con método
+// Enlace de clases: Múltiples clases con objeto
 createApp({
     data() {
-        return { username: '' }
-    },
-    methods: {
-        getWelcomeMessage() {
-            return this.username
-                ? `¡Bienvenido, ${this.username}!`
-                : 'Por favor, escribe tu nombre.';
+        return {
+            isActive: true,
+            hasError: false
         }
     }
-}).mount('#welcome-method');
+}).mount('#class-multi');
+
+// Enlace de clases: Sintaxis de array
+createApp({
+    data() {
+        return {
+            isActive: true,
+            activeClass: 'active',
+            errorClass: 'text-danger'
+        }
+    }
+}).mount('#class-array');
+
+// Enlace de estilos: Sintaxis de objeto
+createApp({
+    data() {
+        return {
+            activeColor: 'red',
+            fontSize: 16
+        }
+    }
+}).mount('#style-object');
+
+// Enlace de estilos: Sintaxis de array
+createApp({
+    data() {
+        return {
+            baseStyles: {
+                color: 'black',
+                fontWeight: 'bold'
+            },
+            overridingStyles: {
+                color: 'blue'
+            }
+        }
+    }
+}).mount('#style-array');
+
+// Enlace de estilos: Valores múltiples
+createApp({}).mount('#style-multi');
