0fa5f673727e130cef0490174792804821ee7ba9
Luis Angel Yuman Rodas <lyumanr@miumg.edu.gt>
2025-09-21 18:30:01 -0600

    Ejercicio Practico Realizado

--- index.js
+++ index.js
@@ -1,87 +1,29 @@
 const { createApp } = Vue;
 
-// Ejemplo 1: Propiedad computada básica
+// Ejemplo con propiedad computada
 createApp({
     data() {
-        return { message: 'Hola' }
+        return { username: '' }
     },
     computed: {
-        reversedMessage() {
-            return this.message.split('').reverse().join('');
+        welcomeMessage() {
+            return this.username
+                ? `¡Bienvenido, ${this.username}!`
+                : 'Por favor, escribe tu nombre.';
         }
     }
-}).mount('#computed-basic');
+}).mount('#welcome-computed');
 
-// Ejemplo 2: Computed vs Método
+// Ejemplo con método
 createApp({
     data() {
-        return { message: 'Vue' }
-    },
-    computed: {
-        reversedMessage() {
-            return this.message.split('').reverse().join('');
-        }
+        return { username: '' }
     },
     methods: {
-        reverseMessage() {
-            return this.message.split('').reverse().join('');
+        getWelcomeMessage() {
+            return this.username
+                ? `¡Bienvenido, ${this.username}!`
+                : 'Por favor, escribe tu nombre.';
         }
     }
-}).mount('#computed-method');
-
-// Ejemplo 3: Computed con getter y setter
-createApp({
-    data() {
-        return {
-            firstName: 'Foo',
-            lastName: 'Bar'
-        }
-    },
-    computed: {
-        fullName: {
-            get() {
-                return this.firstName + ' ' + this.lastName;
-            },
-            set(newValue) {
-                const names = newValue.split(' ');
-                this.firstName = names[0];
-                this.lastName = names.slice(1).join(' ');
-            }
-        }
-    }
-}).mount('#computed-setter');
-
-// Ejemplo 4: Watcher (observador)
-createApp({
-    data() {
-        return {
-            question: '',
-            answer: 'No puedo darte una respuesta hasta que hagas una pregunta!'
-        }
-    },
-    watch: {
-        question(newQuestion) {
-            this.answer = 'Esperando que deje de escribir...';
-            this.debouncedGetAnswer();
-        }
-    },
-    created() {
-        this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
-    },
-    methods: {
-        getAnswer() {
-            if (this.question.indexOf('?') === -1) {
-                this.answer = 'Las preguntas suelen contener un signo de interrogación. ;-)';
-                return;
-            }
-            this.answer = 'Pensando...';
-            axios.get('https://yesno.wtf/api')
-                .then(response => {
-                    this.answer = _.capitalize(response.data.answer);
-                })
-                .catch(error => {
-                    this.answer = '¡Error! No se pudo alcanzar la API. ' + error;
-                });
-        }
-    }
-}).mount('#watch-example');
+}).mount('#welcome-method');
